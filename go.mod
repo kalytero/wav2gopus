fa12307cea277cc1b93870e1ad389e208ee4dc07
yush <yus-sato@kalyte.ro>
2024-08-06 17:16:59 +0000

    Update

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/kalytero/wav2gopus
 go 1.22.5
 
 require gopkg.in/hraban/opus.v2 v2.0.0-20230925203106-0188a62cb302
+
+require github.com/seyukun/gomacros v0.0.1 // indirect
--- wav2gopus.go
+++ wav2gopus.go
@@ -3,56 +3,108 @@ package wav2gopus
 import (
 	"fmt"
 
-	opusv2 "gopkg.in/hraban/opus.v2"
+	Macro "github.com/seyukun/gomacros"
+	Opus "gopkg.in/hraban/opus.v2"
 )
 
-const wav_header_size = 44
+func Encoder(wav_raw []byte, opus_dst *chan []byte, opus_sample_rate int) error {
+	var wav_len int
+	var wav_sample_rate int
+	var pcm_len int
+	var pcm_i16 []int16
+	var err error
 
-// const sampleRate = 24000 // VOICEVOX SampleRate
-// const channels = 1       // VOICEVOX Audio Channel
-// const frameSizeMs = 20
-// const frameSize = sampleRate * frameSizeMs / 1000
+	// wav validation
+	wav_len = len(wav_raw) - 44
+	if wav_len < 0 {
+		return fmt.Errorf("bytes are not wav")
+	}
 
-func Encoder(adapter *opusv2.Encoder, channel *chan []byte, wav []byte, opusFrameSize int) error {
-	var length int
-	var pcm []int16
-	var wav_bytes []byte
+	// wav analysis
+	wav_sample_rate = int(uint16(wav_raw[24]) | uint16(wav_raw[25])<<8)
 
-	var opus_bytes []byte
+	// wav to pcm
+	pcm_i16, pcm_len = wav_to_pcm(wav_raw)
+	pcm_i16, pcm_len = pcm_resample(pcm_i16, pcm_len, wav_sample_rate, opus_sample_rate)
+	// pcm to opus & send to channel
+	err = pcm_to_opus(pcm_i16, pcm_len, opus_dst, opus_sample_rate)
+	if err != nil {
+		return fmt.Errorf("%s(%d) %s", Macro.M__FILE__(), Macro.M__LINE__(), err.Error())
+	}
+	return nil
+}
+
+func wav_to_pcm(wav_raw []byte) ([]int16, int) {
+	var wav_len int
+	var wav_ui8 []uint8
+	var pcm_len int
+	var pcm_i16 []int16
+
+	wav_len = len(wav_raw) - 44
+	wav_ui8 = wav_raw[44:]
+	pcm_len = wav_len / 2
+	pcm_i16 = make([]int16, pcm_len)
+	for i := 0; i < pcm_len; i++ {
+		pcm_i16[i] = int16(wav_ui8[i*2]) + (int16(wav_ui8[i*2+1]) << 8)
+	}
+	return pcm_i16, pcm_len
+}
+
+func pcm_resample(src_pcm []int16, pcm_len, src_rate, dst_rate int) ([]int16, int) {
+	var rate_ratio float64
+	var dst_pcm []int16
+	var src_index float64
+	var l_index int
+	var r_index int
+	var l_value float64
+	var r_value float64
+	var interpolation float64
+
+	rate_ratio = float64(dst_rate) / float64(src_rate)
+	dst_pcm = make([]int16, int(float64(pcm_len)*rate_ratio))
+
+	for i := 0; i < len(dst_pcm); i++ {
+		src_index = float64(i) / rate_ratio
+		l_index = int(src_index)
+		r_index = l_index + 1
+		if r_index >= pcm_len {
+			dst_pcm[i] = src_pcm[l_index]
+		} else {
+			l_value = float64(src_pcm[l_index])
+			r_value = float64(src_pcm[r_index])
+			interpolation = src_index - float64(l_index)
+			dst_pcm[i] = int16(l_value*(1-interpolation) + r_value*interpolation)
+		}
+	}
+	return dst_pcm, len(dst_pcm)
+}
+
+func pcm_to_opus(pcm_i16 []int16, pcm_len int, opus_dst *chan []byte, opus_sample_rate int) error {
+	var opus_encoder *Opus.Encoder
+	var opus_frame_size int
+	var opus_ui8 []uint8
 	var opus_frame []int16
 	var opus_frame_len int
 	var end int
-
 	var err error
 
-	// wav to pcm
-	length = len(wav) - wav_header_size
-	if length < 0 {
-		return fmt.Errorf("bytes are not wav")
+	opus_encoder, err = Opus.NewEncoder(opus_sample_rate, 1, Opus.AppVoIP)
+	if err != nil {
+		return fmt.Errorf("%s(%d) %s", Macro.M__FILE__(), Macro.M__LINE__(), err.Error())
 	}
-	if length%2 == 1 {
-		return fmt.Errorf("wav must be 2 channel")
-	}
-	length = length / 2
-	pcm = make([]int16, length)
-	wav_bytes = wav[44:]
-	for i := 0; i < length; i++ {
-		pcm[i] = int16(wav_bytes[i*2]) + int16(wav_bytes[i*2+1])<<8
-	}
-
-	// pcm to opus & send to channel
-	opus_bytes = make([]byte, opusFrameSize*2)
-	for i := 0; i < length; i += opusFrameSize {
-		end = i + opusFrameSize
-		if i+opusFrameSize > length {
+	opus_frame_size = opus_sample_rate * 20 / 1000
+	opus_ui8 = make([]byte, opus_frame_size*2)
+	for i := 0; i < pcm_len; i += opus_frame_size {
+		if i+opus_frame_size > pcm_len {
 			break
 		}
-		opus_frame = pcm[i:end]
-		opus_frame_len, err = adapter.Encode(opus_frame, opus_bytes)
+		end = i + opus_frame_size
+		opus_frame = pcm_i16[i:end]
+		opus_frame_len, err = opus_encoder.Encode(opus_frame, opus_ui8)
 		if err != nil {
-			return err
+			return fmt.Errorf("%d: %s", Macro.M__LINE__(), err.Error())
 		}
-		*channel <- opus_bytes[:opus_frame_len]
+		*opus_dst <- opus_ui8[:opus_frame_len]
 	}
 	return nil
 }
